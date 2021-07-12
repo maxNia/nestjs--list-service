5b59d7909470735640a08bf5756a899c1960940c
MaxNia <max7little11@gmail.com>
2021-07-12 21:15:00 +0300

    setUp config

---
+++ src/app/constants.ts
@@ -0,0 +1,3 @@
+export const DB_USERNAME = process.env.DB_USERNAME;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_URI = 'mongodb+srv://cluster0.qafom.mongodb.net/n_Contacts?retryWrites=true&w=majority';
