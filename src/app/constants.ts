911d475c97cfccb86500f6a89f497060e4e65c6b
MaxNia <max7little11@gmail.com>
2021-07-12 22:56:42 +0300

    Add contact, refactor contact model/app.module

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
-export const DB_USERNAME = process.env.DB_USERNAME;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_URI = 'mongodb+srv://cluster0.qafom.mongodb.net/n_Contacts?retryWrites=true&w=majority';
+export const DB_USERNAME = 'nikitaITA123';
+export const DB_PASSWORD = '12345';
+export const DB_URI = 'mongodb+srv://cluster0.tvmwu.mongodb.net/n_Contacts?retryWrites=true&w=majority';
