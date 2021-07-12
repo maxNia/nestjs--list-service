1f5d97d5d9e1b81ebfead5cf3ef836c0db3f725a
MaxNia <max7little11@gmail.com>
2021-07-12 20:00:42 +0300

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypegooseModule.forRoot(
-      'mongodb+srv://userList:pgOYaThPmbcJ7gtV@cluster0.qafom.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-    ), 
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
